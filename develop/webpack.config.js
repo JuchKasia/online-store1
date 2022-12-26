4690797e61a2fdd08fc50848b1cf04e193176a3c
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-12-26 15:35:04 +0100

    feat: add asssets

--- develop/webpack.config.js
+++ develop/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -24,6 +25,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+        new ESLintPlugin({ extensions: ['ts', 'js'] }),
 
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
